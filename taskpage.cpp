1ef942e00960d68bf65fb99e9484a4645a8b8fd1
Claudio Scordino <claudio@evidence.eu.com>
2013-07-17 17:25:40 +0200

    GPL license

--- taskpage.cpp
+++ taskpage.cpp
@@ -1,3 +1,21 @@
+/*
+ * Copyright (C) 2013 Claudio Scordino
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
 #include <QStringList>
 #include <QTabWidget>
 #include <QMessageBox>
@@ -84,9 +102,23 @@ void TaskPage::createScene()
 {
 	// Create left scene:
 	QGraphicsScene* taskScene = new QGraphicsScene(this);
-	QGraphicsRectItem *rect = new QGraphicsRectItem();
-	rect->setRect(0, 0, 100, 10);
-	taskScene->addItem(rect);
+	//QGraphicsRectItem *rect = new QGraphicsRectItem();
+
+//	QPainter painter(this);
+//	QColor color = QColor::fromRgb(100, 100, 100);
+//	painter.setPen(color);
+
+	QBrush b (QColor::fromRgb(185, 211, 238));
+
+	QRect* rect = new QRect (0, 0, 20, 800);
+	taskScene->addRect(*rect, palette().foreground().color(), b);
+
+	//QGraphicsTextItem t ()
+	taskScene->addText("Hello, world!");
+	//taskScene->setForegroundBrush(b);
+
+
+
 	view_ = new QGraphicsView(taskScene);
 	view_->show();
 }
